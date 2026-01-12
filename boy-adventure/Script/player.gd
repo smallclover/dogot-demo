57b3ab5142a5a99ae47c76311698d858b7e0e921
smallclover <18363998103@163.com>
2026-01-12 11:30:55 +0800

    黑夜和光照效果

--- boy-adventure/Script/player.gd
+++ boy-adventure/Script/player.gd
@@ -6,6 +6,9 @@ const JUMP_VELOCITY = -400.0
 
 @onready var anime_player: AnimatedSprite2D = $AnimatedSprite2D
 
+var facing : int = 1
+
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -24,6 +27,7 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
+		facing = sign(direction)
 		velocity.x = direction * SPEED
 		if velocity.y == 0:
 			anime_player.play("run")
@@ -32,9 +36,6 @@ func _physics_process(delta: float) -> void:
 		if velocity.y == 0:
 			anime_player.play("idle")
 		
-	if direction == -1:
-		anime_player.flip_h = true
-	else:
-		anime_player.flip_h = false
+	anime_player.flip_h = (facing == -1)
 	
 	move_and_slide()
