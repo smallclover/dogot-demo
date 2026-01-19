effff94a21e5e0fd7f7bf824f6d639f87a7ce882
smallclover <18363998103@163.com>
2026-01-19 16:02:42 +0800

    创建关卡草图

--- rpg-little-adventurer/Game/Script/StateMachine/EnemyState/enemy_move.gd
+++ rpg-little-adventurer/Game/Script/StateMachine/EnemyState/enemy_move.gd
@@ -1,1 +1,38 @@
 extends State
+
+@onready var navigation_agent_2d: NavigationAgent2D = $"../../NavigationAgent2D"
+@onready var player_pos_update_timer: Timer = $"../../PlayerPosUpdateTimer"
+
+var direction: Vector2
+var speed: float
+var SPEED_MAX = 50
+var SPEED_MIN = 30
+func Update():
+	super.Update()
+	character.UpdateAnimation()
+	speed = randf_range(SPEED_MIN, SPEED_MAX)
+
+func UpdatePhysics(delta: float):
+	super.UpdatePhysics(delta)
+	direction = character.global_position.direction_to(navigation_agent_2d.get_next_path_position())
+	
+	if navigation_agent_2d.is_target_reached() == false:
+		character.velocity = character.velocity.lerp(direction * speed, delta)
+		character.move_and_slide()
+
+
+func _on_player_pos_update_timer_timeout() -> void:
+	navigation_agent_2d.target_position = character.player.global_position
+
+func Enter():
+	super.Enter()
+	player_pos_update_timer.start()
+	
+func Exit():
+	super.Exit()
+	player_pos_update_timer.stop()
+
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	if parentStateMachine.currentState == self && navigation_agent_2d.is_target_reached() == false:
+		character.velocity += safe_velocity * get_physics_process_delta_time()
--- rpg-little-adventurer/Game/Script/random_region.gd
+++ rpg-little-adventurer/Game/Script/random_region.gd
@@ -1,11 +1,26 @@
+# 可以使编辑器调用这个脚本
+@tool 
 extends Sprite2D
 
 @export var randomRegion: Array[Rect2]
+# 随机种子控制树木的颜色，保证每次的随机都是一样的
+var seedNumber: int
 
 func _ready() -> void:
 	UpdateRandomRegion()
 
 func UpdateRandomRegion(): 
 	if randomRegion:
+		seedNumber = int(global_position.x + global_position.y)
+		seed(seedNumber)
 		var randomIndex = randi_range(0, randomRegion.size() - 1)
 		region_rect = randomRegion[randomIndex]
+
+func _enter_tree() -> void:
+	# 这个函数只在编辑器里调用
+	set_notify_transform(true)
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_TRANSFORM_CHANGED:
+		if Engine.is_editor_hint():
+			UpdateRandomRegion()
